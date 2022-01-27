0c634bd404c13c92c2def7767472eff7a6eabd5e
Claudio <claudiocli.cp@gmail.com>
2022-01-27 20:29:11 +0100

    Annotations

--- src/app/models/purchase.ts
+++ src/app/models/purchase.ts
@@ -1,14 +1,15 @@
-import { CustomerAccount } from "./customeraccount";
-import { Product } from "./product";
-import { ProductResponse } from "./response/productResponse";
+import { CustomerAccount } from './customeraccount';
+import { Product } from './product';
+import { ProductResponse } from './response/productResponse';
 
 export class Purchase{
     id: number;
     customer: CustomerAccount;
     date: Date;
+    // FIXME: not the same of the backend
     soldProduct: ProductResponse;
     productQuantity: number;
-    shippingAddress : string;
-    paymentMethod : string;
-    total : number;
+    shippingAddress: string;
+    paymentMethod: string;
+    total: number;
 }
